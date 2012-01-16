5387b5926167cbf98a9f083b543c3204c06ff233
Ariel Coppes <ariel.coppes@gemserk.com>
2012-01-16 21:19:30 -0200

    added basic code to integrate with Swarm and added its resources in /res

--- al1-android/src/com/gemserk/games/ludumdare/al1/AndroidApplication.java
+++ al1-android/src/com/gemserk/games/ludumdare/al1/AndroidApplication.java
@@ -1,16 +1,25 @@
 package com.gemserk.games.ludumdare.al1;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gemserk.componentsengine.input.InputDevicesMonitorImpl;
+import com.gemserk.componentsengine.input.LibgdxInputMappingBuilder;
+import com.swarmconnect.Swarm;
+import com.swarmconnect.SwarmLeaderboard;
+import com.swarmconnect.SwarmLeaderboard.GotLeaderboardCB;
 
 public class AndroidApplication extends com.badlogic.gdx.backends.android.AndroidApplication {
 
 	private Game game;
+	private Handler handler;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -29,13 +38,94 @@ public class AndroidApplication extends com.badlogic.gdx.backends.android.Androi
 		config.useCompass = false;
 		config.useWakelock = true;
 
-		game = new Game();
+		handler = new Handler();
+
+		game = new Game() {
+
+			private InputDevicesMonitorImpl<String> inputMonitor;
+
+			@Override
+			public void create() {
+				super.create();
+
+				inputMonitor = new InputDevicesMonitorImpl<String>();
+				new LibgdxInputMappingBuilder<String>(inputMonitor, Gdx.input) {
+					{
+						monitorKey("openDashboard", Keys.MENU);
+					}
+				};
+			}
+
+			@Override
+			public void render() {
+				super.render();
+
+				inputMonitor.update();
+
+				if (inputMonitor.getButton("openDashboard").isReleased()) {
+
+					// handler.post(new Runnable() {
+					//
+					// @Override
+					// public void run() {
+					// // if (!Swarm.isInitialized())
+					// // Swarm.autoLogin(null);
+					//
+					// SwarmLeaderboard.getLeaderboardById(99, new GotLeaderboardCB() {
+					// @Override
+					// public void gotLeaderboard(SwarmLeaderboard leaderboard) {
+					// if (leaderboard != null)
+					// leaderboard.submitScore(100, "super score!");
+					// }
+					// });
+					// }
+					// });
+
+					if (!Swarm.isInitialized()) {
+						Runnable openDashboardRunnable = new Runnable() {
+							@Override
+							public void run() {
+								if (!Swarm.isInitialized())
+									Swarm.init(AndroidApplication.this, 206, "95308f83c2b18956a15289e4064ce8d6");
+							}
+						};
+						handler.post(openDashboardRunnable);
+					} else {
+						
+						SwarmLeaderboard.getLeaderboardById(99, new GotLeaderboardCB() {
+							@Override
+							public void gotLeaderboard(SwarmLeaderboard leaderboard) {
+								if (leaderboard != null)
+									leaderboard.submitScore(100, "super score!");
+							}
+						});
+						
+						 Swarm.showDashboard();
+					}
+
+				}
+
+			}
+
+		};
 
 		View gameView = initializeForView(game, config);
 
 		layout.addView(gameView);
 
 		setContentView(layout);
+
+		Swarm.setActive(AndroidApplication.this);
 	}
-	
+
+	public void onResume() {
+		super.onResume();
+		Swarm.setActive(this);
+	}
+
+	public void onPause() {
+		super.onPause();
+		Swarm.setInactive(this);
+	}
+
 }
