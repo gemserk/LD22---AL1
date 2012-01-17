f906488e56674a0ff65ac38e2a049180c460800d
Ruben Garat <ruben.garat@gemserk.com>
2012-01-17 17:09:04 -0200

    uncommented the box2d debug renderer

--- al1-core/src/main/java/com/gemserk/games/ludumdare/al1/PlayGameState.java
+++ al1-core/src/main/java/com/gemserk/games/ludumdare/al1/PlayGameState.java
@@ -129,7 +129,7 @@ public class PlayGameState extends GameStateImpl {
 
 		scene.addRenderSystem(new RenderableSystem(renderLayers));
 
-//		scene.addRenderSystem(new Box2dRenderSystem(worldCamera, physicsWorld));
+		scene.addRenderSystem(new Box2dRenderSystem(worldCamera, physicsWorld));
 		scene.addRenderSystem(new Box2dLinearVelocityRenderSystem(worldCamera));
 		scene.addRenderSystem(new RenderScriptSystem());
 
